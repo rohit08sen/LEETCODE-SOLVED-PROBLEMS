2f863370a35f110279ea2457f4bbc4b7f59ca602
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2024-04-20 23:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (27.57%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,29 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double result = power(x, n);
+        if (Double.isInfinite(result)) {
+            return Double.NaN; // Handle overflow, return NaN
+        } else {
+            return result;
+        }
+    }
+    
+    private double power(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half = power(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+}
