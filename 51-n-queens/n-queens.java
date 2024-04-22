03d61349abdfc49c216bda455f219de4ba528474
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2024-04-23 00:42:31 +0530

    Time: 2 ms (88.58%) | Memory: 44.8 MB (35.01%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,111 @@
+// class Solution {
+//     public List<List<String>> solveNQueens(int n) {
+//         char board[][]=new char[n][n];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 board[i][j]='.'
+//             }
+//         }
+//         nQn(board,0);
+//     }
+
+//     private static void nQn(char board[][],int row){
+//     //base
+//     if(row==board.length){
+//       printBoard(board);
+//       return;
+//     }
+//     //col loop
+//     for(int j=0;j<board.length;j++){
+//       if(isSafe(board,row,j)){
+//         board[row][j]='Q';
+//         nQn(board,row+1);
+//         board[row][j]='.';
+//       }
+//     }
+//   }
+
+//   private static boolean isSafe(char board[][],int row,int col){
+//     //vertical up
+//     for(int i=row-1;i>=0;i--){
+//       if(board[i][col]=='Q'){
+//         return false;
+//       }
+//     }
+//     //left up
+//     for(int i=row-1, j=col-1;i>=0 &&j>=0;i--,j--){
+//       if(board[i][j]=='Q'){
+//         return false;
+//       }
+//     }
+//     //Right up
+//     for(int i=row-1, j=col+1;i>=0 &&j<board.length;i--,j++){
+//       if(board[i][j]=='Q'){
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+// }
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        nQn(board, 0, solutions);
+        return solutions;
+    }
+
+    private static void nQn(char[][] board, int row, List<List<String>> solutions) {
+        if (row == board.length) {
+            solutions.add(printBoard(board));
+            return;
+        }
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQn(board, row + 1, solutions);
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    private static boolean isSafe(char[][] board, int row, int col) {
+        // Vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<String> printBoard(char[][] board) {
+        List<String> result = new ArrayList<>();
+        for (char[] row : board) {
+            result.add(new String(row));
+        }
+        return result;
+    }
+}
